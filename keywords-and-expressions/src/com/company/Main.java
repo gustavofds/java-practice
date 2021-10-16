42af752b8f6309ca8bac32f5372f786f90d450d9
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-16 16:49:52 -0300

    Keywords, expressions, statements and indentation

---
+++ keywords-and-expressions/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // a mile is equal to 1.609344 kilometers
+        double kilometres = (100 * 1.609344);
+    }
+}
---
+++ statements-white-spaces-and-indentation/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 5;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is another one " + myVariable);
+        // never put statements on the same line
+    }
+}
