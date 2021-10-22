5eb317428ba82f41c85a6f8b04aea1b555bb96b9
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-22 12:31:40 -0300

    Inner Classes - local classes

--- inner-classes/src/com/company/Main.java
+++ inner-classes/src/com/company/Main.java
@@ -1,31 +1,43 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
 
     public static void main(String[] args) {
-	    Gearbox mcLaren = new Gearbox(6);
-        /* OLD WAY:
-        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
-//        Gearbox.Gear second = new Gearbox.Gear(2, 15.4); WRONG
-//        Gearbox.Gear third = new mcLaren.Gear(3, 17.8); WRONG
 
-        System.out.println(first.driveSpeed(1000));
-         */
-        // Better way:
-        mcLaren.operateClutch(true);
-        mcLaren.changeGear(1);
-        mcLaren.operateClutch(false);
-        System.out.println(mcLaren.wheelSpeed(1000));
-        mcLaren.changeGear(2);
-        System.out.println(mcLaren.wheelSpeed(3000));
-        mcLaren.operateClutch(true);
-        mcLaren.changeGear(3);
-        mcLaren.operateClutch(false);
-        System.out.println(mcLaren.wheelSpeed(6000));
+        // Local class
+        class ClickListener implements Button.OnClickListener {
+            public ClickListener() {
+                System.out.println("I've been attached");
+            }
 
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        }
 
+        btnPrint.setOnClickListener(new ClickListener());
+        listen();
 
+    }
 
-
+    private static void listen() {
+        boolean quit = false;
+        while(!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch(choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+                    break;
+            }
+        }
     }
 }
