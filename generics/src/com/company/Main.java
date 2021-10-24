d0fa13e387b8150ff315ac195aa30335fd7e44f5
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-24 19:57:24 -0300

    Generics class

---
+++ generics/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> soccerTeam = new Team<>("Real Madrid");
+        soccerTeam.addPlayer(beckham);
+    }
+
+}
