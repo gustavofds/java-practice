d8f3a4ab31f757e39e517187dea44b13fefb5055
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-28 12:58:46 -0300

    Packages examples with java.awt

---
+++ packages/src/com/company/awtSample/Main.java
@@ -0,0 +1,9 @@
+package com.company.awtSample;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+    }
+}
